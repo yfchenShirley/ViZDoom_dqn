257bc14f33261bb4c632b49798d1a38861fab6c7
yfchenShirley <641751045@qq.com>
2019-05-28 21:35:16 +0200

    modify

--- job1.sh
+++ job1.sh
@@ -7,6 +7,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yifei.chen@rug.nl
 
+module load GCCcore/4.9.3
 module load Python/3.6.4-foss-2018a
 module load TensorFlow/1.12.0-fosscuda-2018a-Python-3.6.4
 module load cuDNN/7.1.4.18-CUDA-9.0.176
@@ -14,10 +15,10 @@ module load CMake/3.8.0-foss-2016a
 
 
 
+
 cd /data/p285442/DeepLearning/ViZDoom_dqn/
 
-
-
+#conda install -c anaconda tensorflow-gpu
 #pip install vizdoom --user
 
 python Deep_Q_learning_with_Doom_hg.py
