e0d46ac44f862f4ea70dbbb0b31618b51675316c
yfchenShirley <641751045@qq.com>
2019-05-28 15:45:52 +0200

    modify file

--- job1.sh
+++ job1.sh
@@ -8,7 +8,10 @@
 #SBATCH --mail-user=yifei.chen@rug.nl
 
 module load Python/3.6.4-foss-2018a
+module load TensorFlow/1.12.0-fosscuda-2018a-Python-3.6.4
 module load cuDNN/7.1.4.18-CUDA-9.0.176
+module load CMake/3.8.0-foss-2016a
+
 
 
 cd /data/p285442/DeepLearning/ViZDoom_dqn/
