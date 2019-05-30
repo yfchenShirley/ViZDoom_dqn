b9396b8bb9376592d5eb4b762ef0c35723f35bcc
yfchenShirley <641751045@qq.com>
2019-05-30 14:12:17 +0200

    install vizdoom

--- job1-cpu.sh
+++ job1-cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=2-10:30:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=8GB
 #SBATCH --job-name=ViZDoom_DQN
 #SBATCH --mail-type=ALL
@@ -11,7 +11,6 @@
 
 cd /data/p285442/DeepLearning/ViZDoom_dqn/
 
-
-#pip install vizdoom --user
+pip install --user vizdoom
 
 python Deep_Q_learning_with_Doom_hg.py
